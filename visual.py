02e224bec0beffc39c38c0f3ec7504f77bfe69fd
rhodez <lostmywaffle@gmail.com>
2025-11-08 20:47:21 +0100

    dev

--- visual.py
+++ visual.py
@@ -1,59 +1,63 @@
 #!/usr/bin/env python3
 
-import os, json
+import os, json, urllib.parse
 
 ROOT = '/var/www/html'
 DATA_JSON = os.path.join(ROOT, 'data', 'visual.json')
+OUT_HTML = os.path.join(ROOT, 'visual.html')
 
 def read_json(path):
     with open(path, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 def h(x):
+    """Escape HTML special chars"""
     return (x or '').replace('&', '&amp;').replace('<', '&lt;').replace('"', '&quot;')
+
+def url(x):
+    """URL-encode spaces and unsafe characters"""
+    return urllib.parse.quote(x, safe="/")
+
+def render_image_item(item):
+    image = item.get('image', '')
+    title = item.get('title', '')
+    return f'''
+<div class="media-item image-item">
+    <div class="media-left">
+        <img src="{url(image)}" alt="{h(os.path.basename(image))}" class="image/png clickable">
+    </div>
+    <div class="media-right">
+        <p class="image-text clickable">{h(title)}</p>
+        <a href="{url(image)}" download class="main__btn">download</a>
+    </div>
+</div>
+'''
 
 def render_video_item(item):
     cover = item.get('cover', '')
     video = item.get('video', '')
     title = item.get('title', '')
-
     return f'''
 <div class="media-item video-item">
     <div class="video-content">
-        <img src="{h(cover)}" alt="cover.png" class="video-cover">
+        <img src="{url(cover)}" alt="cover.png" class="video-cover">
         <video controls class="video-player">
-            <source src="{h(video)}" type="video/mp4">
+            <source src="{url(video)}" type="video/mp4">
         </video>
     </div>
     <p class="video-text">{h(title)}</p>
     <a class="main__btn"
-       onclick="downloadVisual('{h(video)}', '{h(cover)}', this)">
+       onclick="downloadVisual('{url(video)}', '{url(cover)}', this)">
        download
     </a>
 </div>
 '''
 
-def render_image_item(item):
-    image = item.get('image', '')
-    title = item.get('title', '')
-
-    return f'''
-<div class="media-item image-item">
-    <div class="media-left">
-        <img src="{h(image)}" alt="{h(os.path.basename(image))}" class="image/png">
-    </div>
-    <div class="media-right">
-        <p class="image-text">{h(title)}</p>
-        <a href="{h(image)}" download class="main__btn">download</a>
-    </div>
-</div>
-'''
-
 def render_visual_item(item):
-    if isinstance(item, dict) and 'video' in item:
+    """Safely determine if item is image or video"""
+    if isinstance(item, dict) and item.get('type') == 'video':
         return render_video_item(item)
-    else:
-        return render_image_item(item)
+    return render_image_item(item)
 
 def render_html_page(items):
     html_items = '\n'.join([render_visual_item(i) for i in items])
@@ -78,26 +82,14 @@ def render_html_page(items):
         <div class="navbar__container">
             <a href="/" id="navbar__logo">quite classic</a>
             <div class="navbar__toggle" id="mobile-menu">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
+                <span class="bar"></span><span class="bar"></span><span class="bar"></span>
             </div>
             <ul class="navbar__menu">
-                <li class="navbar__item">
-                    <a href="/about.html" class="navbar__links">about</a>
-                </li>
-                <li class="navbar__item">
-                    <a href="/audio.html" class="navbar__links">audio</a>
-                </li>
-                <li class="navbar__item">
-                    <a href="/visual.html" class="navbar__links">visual</a>
-                </li>
-                <li class="navbar__item">
-                    <a href="/other.html" class="navbar__links">other</a>
-                </li>
-                <li class="navbar__item">
-                    <a href="/upload.html" class="navbar__links">* upload</a>
-                </li>
+                <li class="navbar__item"><a href="/about.html" class="navbar__links">about</a></li>
+                <li class="navbar__item"><a href="/audio.html" class="navbar__links">audio</a></li>
+                <li class="navbar__item"><a href="/visual.html" class="navbar__links">* visual</a></li>
+                <li class="navbar__item"><a href="/other.html" class="navbar__links">other</a></li>
+                <li class="navbar__item"><a href="/upload.html" class="navbar__links">upload</a></li>
             </ul>
         </div>
     </nav>
@@ -117,7 +109,7 @@ def main():
     items = [v for _, v in data.items()]
     html = render_html_page(items)
 
-    with open(os.path.join(ROOT, 'visual.html'), 'w', encoding='utf-8') as f:
+    with open(OUT_HTML, 'w', encoding='utf-8') as f:
         f.write(html)
 
 if __name__ == '__main__':
