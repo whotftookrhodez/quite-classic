464ded2be2f859903e50f5e7205ccfdd6096cef9
rhodez <lostmywaffle@gmail.com>
2025-11-10 05:08:03 +0100

    dev

--- data/visual.py
+++ data/visual.py
@@ -10,12 +10,6 @@ data = {
     "untitled_oc_art": {
         "image": "",
         "title": "sea shore - untitled oc art"
-    },
-    "title": {
-        "type": "video",
-        "cover": "/assets/visual/cover.png",
-        "video": "/assets/visual/video.mp4",
-        "title": "artist - title"
     }
 }
 
